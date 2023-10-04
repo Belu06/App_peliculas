aa05abce17e2d4565b1493ff64c94ddff2e8f9b4
Belén Huanca <beluh065@gmail.com>
2023-10-04 12:20:05 -0300

    Arrancando punto 4

--- movies/pelis.py
+++ movies/pelis.py
@@ -3,18 +3,17 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from movies.db import get_db
 
-bp = Blueprint('blog', __name__)
+bp = Blueprint('pelis', __name__)
 
 @bp.route('/')
 def index():
     db = get_db()
     pelis = db.execute(
-        'SELECT f.name, f.title, language_id '
-        ' FROM language l JOIN film f ON l.language_id = f.language_id'
-        ' ORDER BY name ASC'
+        """SELECT l.name AS lenguaje, f.title AS titulo
+           FROM language l JOIN film f ON l.language_id = f.language_id
+           ORDER BY name ASC"""
     ).fetchall()
-    return render_template('blog/index.html', pelis=pelis)
+    return render_template('pelis/index.html', pelis=pelis)
 
