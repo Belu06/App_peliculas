e3dea7778d03fc4c0c88de1f57ed5f73e756f56f
Belén Huanca <beluh065@gmail.com>
2023-10-11 12:16:29 -0300

    Actividad 5 en proceso

--- movies/pelis.py
+++ movies/pelis.py
@@ -15,7 +15,15 @@ def index():
            FROM language l JOIN film f ON l.language_id = f.language_id
            ORDER BY name ASC"""
 
- 
+        """SELECT title AS Película, first_name AS Nombre, last_name AS Apellido
+            FROM actor a JOIN film_actor fa ON a.actor_id = fa.actor_id
+            JOIN film f ON fa.film_id = f.film_id
+            ORDER BY Película ASC"""
+
+        """SELECT c.name AS categoria, title AS Pelicula
+            FROM category c JOIN film_category fc ON c.category_id = fc.category_id
+            JOIN film f ON fc.film_id = f.film_id
+            ORDER BY Pelicula ASC"""
     ).fetchall()
     return render_template('pelis/index.html', pelis=pelis)
 
