6cd14c0ee508fc7edb5be8486bca83416525e80e
Belén Huanca <beluh065@gmail.com>
2023-11-08 12:25:15 -0300

    Actividad 6 en proceso

--- movies/actores.py
+++ movies/actores.py
@@ -12,8 +12,12 @@ bp = Blueprint('actores', __name__,url_prefix="/actor/")
 def index():
     db = get_db()
     actores = db.execute(
-        """SELECT first_name AS Nombre, last_name AS Apellido
-            FROM actor
+        """SELECT first_name AS Nombre, last_name AS Apellido, title AS Pelicula
+            FROM film f JOIN film_actor fa
+            ON f.film_id = fa.film_id
+            JOIN actor a
+            ON fa.actor_id = a.actor_id
+            WHERE 
             ORDER BY Nombre, Apellido ASC"""
     ).fetchall()
     return render_template('actores/index.html', actores=actores)
--- movies/pelis.py
+++ movies/pelis.py
@@ -11,9 +11,16 @@ bp = Blueprint('pelis', __name__)
 def index():
     db = get_db()
     pelis = db.execute(
-        """SELECT title AS Pelicula, first_name AS Nombre, last_name AS Apellido
-            FROM actor a JOIN film_actor fa ON a.actor_id = fa.actor_id
-            JOIN film f ON fa.film_id = f.film_id
+        """SELECT title AS Pelicula, description AS Descripción, rating, first_name AS Nombre_Actor, last_name AS Apellido_Actor, c.name AS Categoria, l.name AS Lenguaje, length AS Duracion 
+            FROM film f JOIN film_category fc
+            ON f.film_id = fc.film_id
+            JOIN category c
+            ON fc.category_id = c.category_id
+            JOIN language l
+            ON f.language_id = l.language_id
+            JOIN film_actor fa ON fa.film_id = f.film_id
+            JOIN actor a ON fa.actor_id = a.actor_id
+            WHERE f.film_id = ?
             ORDER BY Pelicula ASC"""
     ).fetchall()
     return render_template('pelis/index.html', pelis=pelis)
