79798a86b63766cb507f69c3e394ec4b37cdc97c
Belén Huanca <beluh065@gmail.com>
2023-11-08 12:00:59 -0300

    Actividad 5 terminada

--- movies/lenguaje.py
+++ movies/lenguaje.py
@@ -10,17 +10,8 @@ bp = Blueprint('lenguajes', __name__,url_prefix="/lenguaje/")
 @bp.route('/')
 def index():
     db = get_db()
-    lenguaje = db.execute(
-        """SELECT l.name AS lenguaje, f.title AS titulo, release_year AS Año
-           FROM language l JOIN film f ON l.language_id = f.language_id
-           ORDER BY name ASC"""
+    lenguajes = db.execute(
+        """SELECT l.name AS lenguaje FROM language l 
+           ORDER BY lenguaje ASC"""
     ).fetchall()
-    return render_template('lenguaje/index.html', lenguajes=lenguaje)
-
-def get_pelicula(id):
-    lenguaje = get_db().execute(
-        """SELECT *
-            FROM language
-            WHERE language_id = ?,
-            (id,)"""
-    ).fetchone()
+    return render_template('lenguaje/index.html', lenguajes=lenguajes)
