df711442d16ef09b8e515c9a7c2dbca857b1af1e
Belén Huanca <beluh065@gmail.com>
2023-11-22 11:55:22 -0300

    Corregí la actividad 5

--- movies/actores.py
+++ movies/actores.py
@@ -12,12 +12,9 @@ bp = Blueprint('actores', __name__,url_prefix="/actor/")
 def index():
     db = get_db()
     actores = db.execute(
-        """SELECT first_name AS Nombre, last_name AS Apellido, title AS Pelicula
-            FROM film f JOIN film_actor fa
-            ON f.film_id = fa.film_id
-            JOIN actor a
-            ON fa.actor_id = a.actor_id
-            WHERE 
+        """SELECT first_name AS Nombre, last_name AS Apellido
+            FROM actor
             ORDER BY Nombre, Apellido ASC"""
     ).fetchall()
     return render_template('actores/index.html', actores=actores)
+
--- movies/pelis.py
+++ movies/pelis.py
@@ -11,25 +11,25 @@ bp = Blueprint('pelis', __name__)
 def index():
     db = get_db()
     pelis = db.execute(
-        """SELECT title AS Pelicula, description AS Descripción, rating, first_name AS Nombre_Actor, last_name AS Apellido_Actor, c.name AS Categoria, l.name AS Lenguaje, length AS Duracion 
-            FROM film f JOIN film_category fc
-            ON f.film_id = fc.film_id
-            JOIN category c
-            ON fc.category_id = c.category_id
-            JOIN language l
+        """SELECT title AS Pelicula, rating, l.name AS Lenguaje 
+            FROM film f JOIN language l
             ON f.language_id = l.language_id
-            JOIN film_actor fa ON fa.film_id = f.film_id
-            JOIN actor a ON fa.actor_id = a.actor_id
-            WHERE f.film_id = ?
             ORDER BY Pelicula ASC"""
     ).fetchall()
     return render_template('pelis/index.html', pelis=pelis)
 
 def get_pelicula(id):
     pelicula = get_db().execute(
-        """SELECT *
-            FROM film
-            WHERE film_id = ?,
-            (id,)"""
+        """SELECT title AS Pelicula, description AS Descripción, rating, 
+            c.name AS Categoria, l.name AS Lenguaje, length AS Duracion 
+            FROM film f JOIN film_category fc
+            ON f.film_id = fc.film_id
+            JOIN category c
+            ON fc.category_id = c.category_id
+            JOIN language l
+            ON f.language_id = l.language_id
+            WHERE f.film_id = ?,
+            """, (id,)
     ).fetchone()
+    return pelicula
     
